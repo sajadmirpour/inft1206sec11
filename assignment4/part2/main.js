2b0e71932dc638275b02acb09212f7c5a6931b65
liljeju <sajad.m0526@gmail.com>
2025-04-04 21:12:44 -0400

    Declaring the array of image filenames

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -1,3 +1,9 @@
+
+//Name: Sajad Mirpour
+//File: main.js
+//Date: 24 March 2025
+//This file is js for displaying a webpage that displays an image gallery
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,6 +11,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
